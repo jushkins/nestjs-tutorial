87fb1444f8356c39f87dacb52b742db1673b4b75
Jushkin <jushkin.saparmatov@gmail.com>
2023-07-27 12:49:47 +0500

    prisma ORM added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Request } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup() {
+  signup(@Body() dto: any) {
+    console.log({
+      dto,
+    });
+
     return this.authService.signup();
   }
 
